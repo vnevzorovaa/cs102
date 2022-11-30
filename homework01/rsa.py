3a45c910831e4e23fb118737f46c00ef6f613a2e
vnevzorovaa <nevzorova.vr@gmail.com>
2022-11-30 19:51:22 +0300

    Добавлено еще одно решение

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,10 +1,12 @@
+"""importing random"""
 import random
 import typing as tp
 
 
-def is_prime(n: int) -> bool:
+def is_prime(n_n: int) -> bool:
     """
     Tests to see if a number is prime.
+
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -12,44 +14,75 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n_n == 2:
+        return True
+    elif n_n == 1:
+        return False
+    if n_n % 2 == 0:
+        return False
+    d_d = 3
+    while d_d  2 <= n_n and n_n % d_d != 0:
+        d_d += 2
+    return d_d  2 > n_n
+    # pass
 
 
-def gcd(a: int, b: int) -> int:
+# print (is_prime(1))
+
+
+def gcd(a_a: int, b_b: int) -> int:
     """
     Euclid's algorithm for determining the greatest common divisor.
+
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while a_a != 0 and b_b != 0:
+        if a_a > b_b:
+            a_a = a_a % b_b
+        else:
+            b_b = b_b % a_a
+    return a_a + b_b
+    # pass
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
+
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    x, xx, y, yy = 1, 0, 0, 1
+    w = phi
+    while phi:
+        q = e // phi
+        e, phi = phi, e % phi
+        x, xx = xx, x - xx * q
+        y, yy = yy, y - yy * q
+    k = x % w
+    return k
+    # pass
+
+
+#print(multiplicative_inverse(4553, 8268))
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    """test"""
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
@@ -69,20 +102,22 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    """aaaaaaa"""
     # Unpack the key into it's components
     key, n = pk
     # Convert each letter in the plaintext to numbers based on
     # the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
+    cipher = [(ord(char)  key) % n for char in plaintext]
     # Return the array of bytes
     return cipher
 
 
 def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    """error!"""
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char**key) % n) for char in ciphertext]
+    plain = [chr((char  key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,11 @@
+"""importing sth"""
+# import typing as tp
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +14,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    a_a = 0
+    while len(plaintext) > len(keyword):
+        keyword += keyword[a_a]
+        a_a += 1
+    for i, _ in enumerate(keyword):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+        if plaintext[i].isalpha():
+            c_c = ord(plaintext[i])
+            if plaintext[i].isupper() and c_c >= 91 - key:
+                ciphertext += chr(c_c - 26 + key)
+            elif plaintext[i].islower() and c_c >= 123 - key:
+                ciphertext += chr(c_c - 26 + key)
+            else:
+                ciphertext += chr(c_c + key)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +48,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    a_b = 0
+    while len(ciphertext) > len(keyword):
+        keyword += keyword[a_b]
+        a_b += 1
+    for i, _ in enumerate(keyword):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+        if ciphertext[i].isalpha():
+            c_b = ord(ciphertext[i])
+            if ciphertext[i].isupper() and c_b <= 64 + key:
+                plaintext += chr(c_b + 26 - key)
+            elif ciphertext[i].islower() and c_b <= 96 + key:
+                plaintext += chr(c_b + 26 - key)
+            else:
+                plaintext += chr(c_b - key)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
