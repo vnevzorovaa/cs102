28e7f459833b8433ff4ed684a944cb19ba58269e
vnevzorovaa <nevzorova.vr@gmail.com>
2022-12-14 19:24:18 +0300

    die zweite Versuch!

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -25,11 +25,7 @@ def display(grid: tp.List[tp.List[str]]) -> None:
     width = 2
     line = "+".join(["-" * (width * 3)] * 3)
     for row in range(9):
-        print(
-            "".join(
-                grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)
-            )
-        )
+        print("".join(grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)))
         if str(row) in "25":
             print(line)
     print()
