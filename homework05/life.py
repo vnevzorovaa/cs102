db484102aee0adc1f94f16a8ce4bf761a049a881
vnevzorovaa <nevzorova.vr@gmail.com>
2023-03-21 13:26:00 +0300

    123

--- homework05/life.py
+++ homework05/life.py
@@ -170,4 +170,4 @@ class GameOfLife:
                 for j in range(len(self.curr_generation)):
                     row += str(self.curr_generation[i][j])
                 file.write(row)
-                #done
+               
