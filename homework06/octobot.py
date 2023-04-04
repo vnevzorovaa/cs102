7f9f7cc2b1ce8ced28d7ffbde9507faed7332e3d
vnevzorovaa <nevzorova.vr@gmail.com>
2023-04-04 14:40:58 +0300

    Добавлено решение

--- homework06/octobot.py
+++ homework06/octobot.py
@@ -7,8 +7,7 @@ from datetime import datetime, timedelta  # type: ignore
 import gspread  # type: ignore
 import pandas as pd  # type: ignore
 import telebot  # type: ignore
-
-from dateutil.parser import parse # type: ignore
+from dateutil.parser import parse  # type: ignore
 from google.oauth2.gdch_credentials import ServiceAccountCredentials
 
 bot = telebot.TeleBot("6007313686:AAGmqO01oyg2E1CyFfTKMONrx__6udFs4Lk")
