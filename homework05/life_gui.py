c2aef3e0fbf12fbbd276c35bcc54aa841f223d8c
vnevzorovaa <112022861+vnevzorovaa@users.noreply.github.com>
2023-05-25 19:51:53 +0300

    Update life_gui.py moree

--- homework05/life_gui.py
+++ homework05/life_gui.py
@@ -22,9 +22,7 @@ class GUI(UI):
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(
-                self.screen, pygame.Color("black"), (0, y), (self.width, y)
-            )
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
         grid = self.life.curr_generation
