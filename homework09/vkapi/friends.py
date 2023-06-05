080f92764a76346aa46568822e5f8b5e3e27dd81
vnevzorovaa <112022861+vnevzorovaa@users.noreply.github.com>
2023-06-05 21:40:03 +0300

    Update friends.py

--- homework09/vkapi/friends.py
+++ homework09/vkapi/friends.py
@@ -4,7 +4,7 @@ import time
 import typing as tp
 
 from vkapi import config
-from vkapi.exceptions import APIError
+
 from vkapi.session import Session
 
 QueryParams = tp.Optional[tp.Dict[str, tp.Union[str, int]]]
